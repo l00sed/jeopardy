b1b220f447d89c3c67205479daa4bfe6f9621510
l00sed <danielwtompkins@gmail.com>
2020-12-28 22:39:57 -0500

    added automation of daily-doubles (1 in jeopardy round, 2 in double jeopardy) in formatter_new.py

--- formatter_new.py
+++ formatter_new.py
@@ -1,4 +1,5 @@
 import os
+import random
 import json
 import argparse
 from ftfy import fix_text
@@ -12,6 +13,15 @@ with open(args.source_file) as data_file:
     game_data = json.load(data_file)
 
 rounds = ('jeopardy', 'double-jeopardy', 'final-jeopardy')
+
+options = [3,4,5,8,9,10,13,14,15,18,19,20,23,24,25,28,29,30]
+
+daily_double1 = random.choice(options)
+options.remove(daily_double1)
+daily_double2 = random.choice(options)
+options.remove(daily_double2)
+daily_double3 = random.choice(options)
+options.remove(daily_double3)
 
 jeopardy_categories = ('PREFIXES', 'PHRASES OF DEATH', 'PIZZA TOPPINGS', 'PITS BURG', 'PRAWNOGRAPHY', 'ADJECTIVES')
 jeopardy_show = ('5308', '6261', '4936', '4645', '4908', '5126')
@@ -28,25 +38,35 @@ formatted_data = { "jeopardy": [], "double-jeopardy": [], "final-jeopardy": {} }
 for round in rounds:
     if round == 'jeopardy':
         round_formatted = 'Jeopardy!'
+        i=1
         for category in jeopardy_categories:
             category_object = { "name": category, "questions": [] }
             for value in jeopardy_values:
                 for show_number in jeopardy_show:
                     for option in game_data:
                         if option['round'] == round_formatted and option['category'] == category and option['show_number'] == show_number and option['value'] == '$'+str(value):
-                            category_object["questions"].append({ "value": value, "question": fix_text(option['question']), "answer": fix_text(option['answer']) })
+                            if value >= 600 and i == daily_double1:
+                                category_object["questions"].append({ "value": value, "question": fix_text(option['question']), "answer": fix_text(option['answer']), "daily-double": "true" })
+                            else:
+                                category_object["questions"].append({ "value": value, "question": fix_text(option['question']), "answer": fix_text(option['answer']) })
+                            i+=1
             formatted_data["jeopardy"].append(category_object)
     print(formatted_data)
 
     if round == 'double-jeopardy':
         round_formatted = 'Double Jeopardy!'
+        i=1 
         for category in double_jeopardy_categories:
             category_object = { "name": category, "questions": [] }
             for value in double_jeopardy_values:
                 for show_number in double_jeopardy_show:
                     for option in game_data:
                         if option['round'] == round_formatted and option['category'] == category and option['show_number'] == show_number and option['value'] == '$'+str(value):
-                            category_object["questions"].append({ "value": value, "question": fix_text(option['question']), "answer": fix_text(option['answer']) })
+                            if value >= 1200 and (i == daily_double2 or i == daily_double3):
+                                category_object["questions"].append({ "value": value, "question": fix_text(option['question']), "answer": fix_text(option['answer']), "daily-double": "true" })
+                            else:
+                                category_object["questions"].append({ "value": value, "question": fix_text(option['question']), "answer": fix_text(option['answer']) })
+                            i+=1
             formatted_data["double-jeopardy"].append(category_object)
     print(formatted_data)
 
